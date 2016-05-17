b256480bba4da35225dd2b9b2900124429c0d7f5
Krasiyan <hello@krasiyan.com>
2016-05-17 18:54:55 +0300

    switched lodash.get to lodash.has for perfomance/simplicty reasons

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var fold = require("organic-dna-fold")
-var get = require("lodash.get")
+var has = require("lodash.has")
 var selectBranch = require("organic-dna-branches").selectBranch
 
 module.exports = function(dna, value){
   var modes = value.split("+")
   modes.forEach(function(m){
-    if(dna[m] || get(dna, m))
+    if(dna[m] || has(dna, m))
       fold(dna, selectBranch(dna, m))
     else
       console.warn("mode ",m," not found in root dna")
